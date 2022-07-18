7fe33b1495b7c45602a80a302755ea8944f0eb5d
vitorsr116 <45939283+vitorsr116@users.noreply.github.com>
2022-07-18 07:40:24 -0300

    fanfiqueiro DE MERDA

--- super-veterano/src/scenes/screens/TextHistory/index.js
+++ super-veterano/src/scenes/screens/TextHistory/index.js
@@ -1,6 +1,13 @@
 import Phaser from "phaser";
 
-const initialHistory = ["Era uma vez um garoto cahamado 'Luiz'.", "Ele era um estudante de Computação\ndo Instituto Federal de Brasília\nque passava por um grande problema \nem sua carreira acadêmica.", "Ele era terrível na arte de somar!\n\n1+1 = 3 ???\nPara ele isso fazia sentido."];
+const initialHistory = ["Era uma vez um garoto chamado 'Luiz'.",
+"Ele era um estudante de Computação\ndo Instituto Federal de Brasília\nque passava por um terrível problema \nem sua carreira acadêmica.", 
+"Ele era terrível em matemática e já não sabia o que fazer\n\npensou até em trancar o curso por causa desse problema.",
+"Mas, como um super veterano\n ele tinha que dá um exemplo ao seus calouros\n enfrentar o seu grande medo.",
+"O Rei da integral dupla\n Seu grande professor\n Dhiego. ",
+"Para ajudar ele nessa linda jornada\n ele pensou em criar um jogo para ajudar a estudar.  ",
+"Com isso, apresentamos\n Super Veterano.  ",
+];
 let index = 0;
 
 export default class TextHitory extends Phaser.Scene {
